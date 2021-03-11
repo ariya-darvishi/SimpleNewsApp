79fbb05edb1d1a83893f2936bf58278c844ea344
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-11 10:08:41 +0330

    Setup Retrofit :)

---
+++ app/src/main/java/com/example/simplenewsapp/data/local/models/Source.kt
@@ -0,0 +1,6 @@
+package com.example.simplenewsapp.data.local.models
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/example/simplenewsapp/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.simplenewsapp.util
+
+class Constants {
+
+    companion object{
+
+        const val API_KEY = "8dee32dd94c043668c895c091006cbaa"
+        const val BASE_URL = "https://newsapi.org"
+
+        const val DATABASE_NAME = "article_db.db"
+
+    }
+}
